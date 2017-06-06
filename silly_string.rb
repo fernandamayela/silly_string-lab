a81d16f1de08ad9e4fa268b189d5bd284791f607
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2017-06-06 15:24:27 +0000

    finished silly string lab

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,16 @@
-#code your solutions here
+my_word = "pistachio"
+puts my_word.upcase
 
+name = "IM NOT SHOUTING"
+puts name.downcase
 
+my_name = "Karlie Kloss" + " Kode With Klossy"
+puts my_name.length
+
+puts "wow" + " mom"
+
+name = "I really like progamming"
+puts name.capitalize
+
+phrase = ""Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!""
 
